e7b5c1ec0e955c1191e2f199b5a924973621107b
Dmitry Platonov <dmitry.platonov@gmail.com>
2018-11-29 09:43:39 +0300

    Rearrange code

--- src/Main.purs
+++ src/Main.purs
@@ -9,13 +9,6 @@ import Data.Lens.Record (prop)
 import Data.Symbol (SProxy(..))
 
 type CounterModel = Int
-type Model = { counter1:: CounterModel, counter2:: CounterModel }
-type ModelUpdater = Model -> Model
-data Msg = Msg ModelUpdater
-
-init :: Model
-init = { counter1: 0, counter2: 0 }
-
 data CounterMsg = Increment | Decrement | Reset
 
 counterUpdate :: CounterMsg -> CounterModel -> CounterModel
@@ -23,6 +16,23 @@ counterUpdate msg model = case msg of
   Increment -> model + 1
   Decrement -> model - 1
   Reset -> 0
+
+counter :: CounterModel -> H.Html CounterMsg
+counter model = H.div [] [
+  button [onClick Decrement] [text "-"],
+  text (show model),
+  button [onClick Increment] [text "+"]
+]
+
+boundCounter :: (L.Lens' Model CounterModel) -> Model -> H.Html Msg
+boundCounter lens model =
+  (\msg -> Msg (L.over lens (counterUpdate msg))) <$> counter (L.view lens model)
+type Model = { counter1:: CounterModel, counter2:: CounterModel }
+type ModelUpdater = Model -> Model
+data Msg = Msg ModelUpdater
+
+init :: Model
+init = { counter1: 0, counter2: 0 }
 
 counter1Lens :: L.Lens' Model CounterModel
 counter1Lens = prop (SProxy:: SProxy "counter1")
@@ -34,21 +44,10 @@ update :: Model -> Msg -> Model
 update model = case _ of
   Msg transformer -> transformer model
 
-boundCounter :: (L.Lens' Model CounterModel) -> Model -> H.Html Msg
-boundCounter lens model =
-  (\msg -> Msg (L.over lens (counterUpdate msg))) <$> counter (L.view lens model)
-
 view :: Model -> H.Html Msg
 view model = H.main [] $ (flip ($) model) <$> [
   boundCounter counter1Lens,
   boundCounter counter2Lens
-]
-
-counter :: CounterModel -> H.Html CounterMsg
-counter model = H.div [] [
-  button [onClick Decrement] [text "-"],
-  text (show model),
-  button [onClick Increment] [text "+"]
 ]
 
 main :: Effect Unit
